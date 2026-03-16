7e252e36ecfb6a6bcc165a87ea0497609dcfcc50
ThitipongPP <thitipong.pgpt@gmail.com>
2026-03-16 23:31:31 +0700

    Making parser and statement

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,1 +1,41 @@
 package parser
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Cutting a cli command to be statement
+func Parse(cli []string) (*Statement, error) {
+	// Don't have a command
+	if len(cli) < 2 {
+		return nil, fmt.Errorf("not enough arguments: missing command")
+	}
+
+	cli = cli[1:]
+	statement := NewStatement(cli[0])
+
+	// Don't have any arguments
+	if len(cli) == 1 {
+		return statement, nil
+	}
+
+	cli = cli[1:]
+	for i:=0; i<len(cli); i++ {
+		switch {
+		case strings.HasPrefix(cli[i], "--"):
+			statement.SetBoolFlag(cli[i])
+		case strings.HasPrefix(cli[i], "-"):
+			if i+1 >= len(cli) || strings.HasPrefix(cli[i+1], "-") {
+				return nil, fmt.Errorf("missing required value for flag: %s", cli[i])
+			}
+
+			statement.PushFlag(cli[i], cli[i+1])
+			i++
+		default:
+			statement.Args = append(statement.Args, cli[i])
+		}
+	}
+	
+	return statement, nil
+}
--- internal/parser/statement.go
+++ internal/parser/statement.go
@@ -1,1 +1,49 @@
 package parser
+
+import (
+	"strings"
+)
+
+type Statement struct{
+	Command string
+	Args 	[]string
+	Flags 	map[string][]string
+	BoolFlags map[string]bool
+}
+
+// Create new statement
+func NewStatement(command string) (*Statement) {
+	return &Statement{
+		Command: 	command,
+		Args: 		make([]string, 0),
+		Flags: 		make(map[string][]string),
+		BoolFlags: 	make(map[string]bool),
+	}
+}
+
+// Set bool flag to true
+func (s *Statement) SetBoolFlag(flag string) {
+	flag = strings.TrimPrefix(flag, "--")
+	s.BoolFlags[flag] = true
+}
+
+// Set flag to value
+func (s *Statement) PushFlag(flag string, item string) {
+	// Split item to item list
+	itemList := strings.Split(item, ",")
+	for i := range itemList {
+		itemList[i] = strings.TrimSpace(itemList[i])
+	}
+
+	flag = strings.TrimPrefix(flag, "-")
+
+	// Finding data in flag
+	data, finding := s.Flags[flag]
+	if !finding {
+		s.Flags[flag] = make([]string, 0, len(itemList))
+	}
+
+	// Setting data to flag
+	data = append(data, itemList...)
+	s.Flags[flag] = data
+}
